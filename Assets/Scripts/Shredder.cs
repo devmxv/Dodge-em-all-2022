5d7e903b615c9304312c626282935901c09e69da
devmxv <devmxv@gmail.com>
2022-11-02 20:45:49 -0600

    First commit after 1.0 release

---
+++ Assets/Scripts/Shredder.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//-----------------
+// Shredder.cs
+// Allows to destroy the enemies that are off screen
+//-----------------
+
+public class Shredder : MonoBehaviour
+{
+    [SerializeField] AudioClip playerDieSFX;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        AudioSource.PlayClipAtPoint(playerDieSFX, Camera.main.transform.position);
+        Destroy(collision.gameObject);
+        FindObjectOfType<Level>().showGameOver();
+    }
+}
